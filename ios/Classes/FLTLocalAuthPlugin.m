81d29cf65b52fe59fbc2bcfc11830dd9fcce7094
Ngọc Đỗ <ngocdtph03070@gmail.com>
2021-04-28 17:40:08 +0700

    Update FLTLocalAuthPlugin.m

--- ios/Classes/FLTLocalAuthPlugin.m
+++ ios/Classes/FLTLocalAuthPlugin.m
@@ -1,4 +1,4 @@
-// Copyright 2013 The Flutter Authors. All rights reserved.
+// Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 #import <LocalAuthentication/LocalAuthentication.h>
@@ -6,17 +6,11 @@
 #import "FLTLocalAuthPlugin.h"
 
 @interface FLTLocalAuthPlugin ()
-@property(nonatomic, copy, nullable) NSDictionary<NSString *, NSNumber *> *lastCallArgs;
-@property(nonatomic, nullable) FlutterResult lastResult;
-// For unit tests to inject dummy LAContext instances that will be used when a new context would
-// normally be created. Each call to createAuthContext will remove the current first element from
-// the array.
-- (void)setAuthContextOverrides:(NSArray<LAContext *> *)authContexts;
+@property(copy, nullable) NSDictionary<NSString *, NSNumber *> *lastCallArgs;
+@property(nullable) FlutterResult lastResult;
 @end
 
-@implementation FLTLocalAuthPlugin {
-  NSMutableArray<LAContext *> *_authContextOverrides;
-}
+@implementation FLTLocalAuthPlugin
 
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar> *)registrar {
   FlutterMethodChannel *channel =
@@ -28,36 +22,16 @@
 }
 
 - (void)handleMethodCall:(FlutterMethodCall *)call result:(FlutterResult)result {
-  if ([@"authenticate" isEqualToString:call.method]) {
-    bool isBiometricOnly = [call.arguments[@"biometricOnly"] boolValue];
-    if (isBiometricOnly) {
-      [self authenticateWithBiometrics:call.arguments withFlutterResult:result];
-    } else {
-      [self authenticate:call.arguments withFlutterResult:result];
-    }
+  if ([@"authenticateWithBiometrics" isEqualToString:call.method]) {
+    [self authenticateWithBiometrics:call.arguments withFlutterResult:result];
   } else if ([@"getAvailableBiometrics" isEqualToString:call.method]) {
     [self getAvailableBiometrics:result];
-  } else if ([@"isDeviceSupported" isEqualToString:call.method]) {
-    result(@YES);
   } else {
     result(FlutterMethodNotImplemented);
   }
 }
 
 #pragma mark Private Methods
-
-- (void)setAuthContextOverrides:(NSArray<LAContext *> *)authContexts {
-  _authContextOverrides = [authContexts mutableCopy];
-}
-
-- (LAContext *)createAuthContext {
-  if ([_authContextOverrides count] > 0) {
-    LAContext *context = [_authContextOverrides firstObject];
-    [_authContextOverrides removeObjectAtIndex:0];
-    return context;
-  }
-  return [[LAContext alloc] init];
-}
 
 - (void)alertMessage:(NSString *)message
          firstButton:(NSString *)firstButton
@@ -94,7 +68,7 @@
 }
 
 - (void)getAvailableBiometrics:(FlutterResult)result {
-  LAContext *context = self.createAuthContext;
+  LAContext *context = [[LAContext alloc] init];
   NSError *authError = nil;
   NSMutableArray<NSString *> *biometrics = [[NSMutableArray<NSString *> alloc] init];
   if ([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics
@@ -118,7 +92,7 @@
 
 - (void)authenticateWithBiometrics:(NSDictionary *)arguments
                  withFlutterResult:(FlutterResult)result {
-  LAContext *context = self.createAuthContext;
+  LAContext *context = [[LAContext alloc] init];
   NSError *authError = nil;
   self.lastCallArgs = nil;
   self.lastResult = nil;
@@ -129,68 +103,30 @@
     [context evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics
             localizedReason:arguments[@"localizedReason"]
                       reply:^(BOOL success, NSError *error) {
-                        dispatch_async(dispatch_get_main_queue(), ^{
-                          [self handleAuthReplyWithSuccess:success
-                                                     error:error
-                                          flutterArguments:arguments
-                                             flutterResult:result];
-                        });
+                        if (success) {
+                          result(@YES);
+                        } else {
+                          switch (error.code) {
+                            case LAErrorPasscodeNotSet:
+                            case LAErrorTouchIDNotAvailable:
+                            case LAErrorTouchIDNotEnrolled:
+                            case LAErrorTouchIDLockout:
+                              [self handleErrors:error
+                                   flutterArguments:arguments
+                                  withFlutterResult:result];
+                              return;
+                            case LAErrorSystemCancel:
+                              if ([arguments[@"stickyAuth"] boolValue]) {
+                                self.lastCallArgs = arguments;
+                                self.lastResult = result;
+                                return;
+                              }
+                          }
+                          result(@NO);
+                        }
                       }];
   } else {
     [self handleErrors:authError flutterArguments:arguments withFlutterResult:result];
-  }
-}
-
-- (void)authenticate:(NSDictionary *)arguments withFlutterResult:(FlutterResult)result {
-  LAContext *context = self.createAuthContext;
-  NSError *authError = nil;
-  _lastCallArgs = nil;
-  _lastResult = nil;
-  context.localizedFallbackTitle = @"";
-
-  if (@available(iOS 9.0, *)) {
-    if ([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthentication error:&authError]) {
-      [context evaluatePolicy:kLAPolicyDeviceOwnerAuthentication
-              localizedReason:arguments[@"localizedReason"]
-                        reply:^(BOOL success, NSError *error) {
-                          dispatch_async(dispatch_get_main_queue(), ^{
-                            [self handleAuthReplyWithSuccess:success
-                                                       error:error
-                                            flutterArguments:arguments
-                                               flutterResult:result];
-                          });
-                        }];
-    } else {
-      [self handleErrors:authError flutterArguments:arguments withFlutterResult:result];
-    }
-  } else {
-    // Fallback on earlier versions
-  }
-}
-
-- (void)handleAuthReplyWithSuccess:(BOOL)success
-                             error:(NSError *)error
-                  flutterArguments:(NSDictionary *)arguments
-                     flutterResult:(FlutterResult)result {
-  NSAssert([NSThread isMainThread], @"Response handling must be done on the main thread.");
-  if (success) {
-    result(@YES);
-  } else {
-    switch (error.code) {
-      case LAErrorPasscodeNotSet:
-      case LAErrorTouchIDNotAvailable:
-      case LAErrorTouchIDNotEnrolled:
-      case LAErrorTouchIDLockout:
-        [self handleErrors:error flutterArguments:arguments withFlutterResult:result];
-        return;
-      case LAErrorSystemCancel:
-        if ([arguments[@"stickyAuth"] boolValue]) {
-          self->_lastCallArgs = arguments;
-          self->_lastResult = result;
-          return;
-        }
-    }
-    result(@NO);
   }
 }
 
@@ -211,11 +147,12 @@
       errorCode = authError.code == LAErrorPasscodeNotSet ? @"PasscodeNotSet" : @"NotEnrolled";
       break;
     case LAErrorTouchIDLockout:
-      [self alertMessage:arguments[@"lockOut"]
-               firstButton:arguments[@"okButton"]
-             flutterResult:result
-          additionalButton:nil];
-      return;
+      dispatch_async(dispatch_get_main_queue(), ^{
+       [self alertMessage:arguments[@"lockOut"]
+          firstButton:arguments[@"okButton"]
+         flutterResult:result
+       additionalButton:nil];
+     });
   }
   result([FlutterError errorWithCode:errorCode
                              message:authError.localizedDescription
